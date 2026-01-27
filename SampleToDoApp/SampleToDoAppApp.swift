d4cd1285418f8250889adc1cfd6830202a45273e
Rudraksh Chourey <Rudrakshchourey@outlook.com>
2026-01-27 12:26:58 -0800

    Initial Commit

---
+++ SampleToDoApp/SampleToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleToDoAppApp.swift
+//  SampleToDoApp
+//
+//  Created by rentamac on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SampleToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
